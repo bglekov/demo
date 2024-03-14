83748e53eef5690ff47233052b4b73210bd90fce
borich <b.glekov@gmail.com>
2024-03-14 14:54:09 +0000

    fix2 diagnos, doctor

--- hr_hospital/models/hh_diagnos.py
+++ hr_hospital/models/hh_diagnos.py
@@ -1,16 +1,20 @@
-from odoo import api, models, fields
+from odoo import models, fields
 
 
 class Diagnos(models.Model):
     _name = 'hh.diagnos'
     _description = 'Diagnos'
 
-    visit_id = fields.Many2one(comodel_name='hh.patient.visit',string="Visit",required=True)
+    visit_id = fields.Many2one(
+        comodel_name='hh.patient.visit',
+        string="Visit",
+        required=True
+    )
     illness_id = fields.Many2one(comodel_name='hh.illness')
     appointment = fields.Char(string="Appointment for treatment")
     approved = fields.Boolean()
 
-    #view reference fields
+    # view reference fields
     doctor_id = fields.Many2one(
         comodel_name='hh.doctor',
         related='visit_id.doctor_id',
--- hr_hospital/models/hh_doctor.py
+++ hr_hospital/models/hh_doctor.py
@@ -14,13 +14,12 @@ class Doctor(models.Model):
         comodel_name='hh.speciality',
     )
     is_intern = fields.Boolean()
-    doctor_mentor = fields.Many2one(comodel_name='hh.doctor',
-        domain="[('is_intern', '=', False),('id', '!=', id),]"
+    doctor_mentor = fields.Many2one(
+        comodel_name='hh.doctor',
+        domain="[('is_intern', '=', False),('id', '!=', id),]",
     )
 
-
-    @api.onchange('is_intern','doctor_mentor')
+    @api.onchange('is_intern', 'doctor_mentor')
     def _onchange_is_intern(self):
-        #for rec in self:
-            if not self.is_intern:
-                self.doctor_mentor = None
+        if not self.is_intern:
+           self.doctor_mentor = None
